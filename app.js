1d7ad6d5d31b164c4ed3ccaf86b4ba1c1f10ed77
qq912701301 <zhuguihao@163.com>
2017-12-02 14:34:59 +0800

    scanPage
    扫码

--- app.js
+++ app.js
@@ -17,16 +17,21 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log("login:"+JSON.stringify(res));
+        
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log("getSetting:" + JSON.stringify(res));
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
+              console.log("getUserInfo-res:" + JSON.stringify(res));
+              console.log("getUserInfo:" + JSON.stringify(res.userInfo));
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
 
