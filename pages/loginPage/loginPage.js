9adf8b109f76967b6e758abc4f853c3bd0074210
qq912701301 <zhuguihao@163.com>
2017-12-05 23:39:04 +0800

    login

--- pages/loginPage/loginPage.js
+++ pages/loginPage/loginPage.js
@@ -1,4 +1,5 @@
 // pages/loginPage/loginPage.js
+const util = require('../../utils/util.js')
 Page({
 
   /**
@@ -8,13 +9,14 @@ Page({
     /**
      * 登录账号
      */
-    account: ""
+    account1: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
     console.log(options);
     if (options.page) {
       var page = options.page;
@@ -47,15 +49,44 @@ Page({
    * 输入框绑定值
    */
   bindAccountInput: function (e) {
-    this.setData({
-      account: e.detail.value
-    })
+    var vm = this;
+    var pos = e.detail.cursor;
+    var value = e.detail.value;
+    var regValue = value.substr(pos - 1, pos);
+    console.log(pos);
+    console.log(value);
+    var account = util.regAccount(regValue, "account");
+    console.log(value.splice(pos,1, account));
+    return {
+      value: account,
+      cursor: pos
+    }
   },
+  /**
+   * 输入框改变值
+   */
+  replace: function (e) {
+    var value = e.detail.value
+    var pos = e.detail.cursor
+    if (pos != -1) {
+      //光标在中间
+      var left = e.detail.value.slice(0, pos)
+      //计算光标的位置
+      pos = left.replace(/11/g, '2').length
+    }
+
+    //直接返回对象，可以对输入进行过滤处理，同时可以控制光标的位置
+    return {
+      value: value.replace(/11/g, '2'),
+      cursor: pos
+    }
+  },
+
   /**
    * 登录
    */
   bindLogin: function () {
-    wx.redirectTo({
+    wx.reLaunch({
       url: '../homePage/homePage',
     })
   },
--- utils/util.js
+++ utils/util.js
@@ -13,6 +13,43 @@ const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
-module.exports = {
-  formatTime: formatTime
+
+/**
+   * 校验只能输入中文，英文和 @
+   */
+// const regAccount = function (account) {
+//   console.log(123123123123);
+//   console.log(account);
+//   var reg = new RegExp('/^[0-9a-zA-Z]+$/', 'g');
+//   console.log(reg);
+//   console.log(reg.exec(account));
+// }
+
+const check = function (account, type) {
+  var regLowerCase = new RegExp('[a-z]', 'g');//判断用户输入的是否为小写字母
+  var regCapitalLetter = new RegExp('[A-Z]', 'g');//判断用户输入的是否为大写字母
+  var regNum = new RegExp('[0-9]', 'g');//判断用户输入的是否为数字
+  var regCom = new RegExp('.*@.*', 'g');//判断用户输入的是否为 @
+  var regD = new RegExp('(\\.)', 'g');//判断用户输入的是否为小数点
+  var rsLowerCase = regLowerCase.exec(account);
+  var rsCapitalLetter = regCapitalLetter.exec(account);
+  var rsNum = regNum.exec(account);
+  var rsCom = regCom.exec(account);
+  var rsD = regD.exec(account);
+  var result = "";
+  switch (type) {
+    case "account":
+      if (rsLowerCase || rsCapitalLetter || rsNum || rsCom || rsD) {
+        result = account;
+      }
+      break;
+  }
+  console.log(account);
+  return account;
 }
+
+module.exports = {
+  formatTime: formatTime,
+  regAccount: check
+}
+
